1baff8aad03287257afc75a5d987a8f4d5249aba
adamelmore <elmore.adam@gmail.com>
2022-11-23 09:05:56 -0600

    fix: social meta tags

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,29 +1,10 @@
-import Head from "next/head"
 import { ComponentProps } from "react"
 import Nav from "./nav"
-
-const title = "awguess"
-const description = "Everyone's favorite AWS guessing game"
 
 export const Layout: React.FC<ComponentProps<"div">> = ({ children }) => {
   return (
     <>
-      <Head>
-        <title>{`${title} | ${description}`}</title>
-        <meta name="description" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:site_name" content="awguess" />
-        <meta property="og:locale" content="en_US" />
-        <meta property="og:url" content="https://awguess.com" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:image" content="https://awguess.com/banner.png" />
-        <meta name="twitter:site" content="@adamdotdev" />
-      </Head>
-
       <Nav />
-
       <main className="container mx-auto max-w-sm md:max-w-3xl p-8 text-orange-9">
         {children}
       </main>
