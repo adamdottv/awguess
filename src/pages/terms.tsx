2beadd2d3605fb7ce5ca9a4d054a4384b42fba8f
Adam Elmore <adam@Kaceys-MacBook-Pro.local>
2022-11-29 06:46:20 -0800

    fix: terms

---
+++ src/pages/terms.tsx
@@ -0,0 +1,20 @@
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from "next"
+import React from "react"
+import { Layout } from "../components/layout"
+import Nav from "../components/nav"
+
+function Terms() {
+  return (
+    <Layout>
+      <Nav />
+      <main className="container mx-auto max-w-sm md:max-w-3xl p-8 text-orange-9">
+        <h1 className="text-3xl font-display font-black">Terms of Service</h1>
+        <ul>
+          <li>no bots, k?</li>
+        </ul>
+      </main>
+    </Layout>
+  )
+}
+
+export default Terms
